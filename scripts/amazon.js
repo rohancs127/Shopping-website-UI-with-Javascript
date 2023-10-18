0a85c3f741c5730841867176ba7c871e804d467e
Rohan <rohankumar5803@gmail.com>
2023-10-18 20:19:59 +0530

    amazon.js file created and updated

---
+++ scripts/amazon.js
@@ -0,0 +1,28 @@
+const products =[{
+    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating: {
+        stars: 4.5,
+        count: 87
+    },
+    price: 1090
+},
+{
+    image: 'images/products/intermediate-composite-basketball.jpg',
+    name: 'Intermediate Size Basketball',
+    rating:{
+        stars: 4,
+        count: 127
+    },
+    price: 2095
+},
+{
+    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+    rating:{
+        stars: 4.9,
+        count: 56
+    },
+    price: 799
+}
+]
