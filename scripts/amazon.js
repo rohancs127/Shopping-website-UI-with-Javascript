6ca99e22d8e83828bf9b5a719d62cf234c4e3db2
Rohan <rohankumar5803@gmail.com>
2023-10-19 20:18:30 +0530

    products.js file attached

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,3 @@
-const products =[{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    price: 1090
-},
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating:{
-        stars: 4,
-        count: 127
-    },
-    price: 2095
-},
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        stars: 4.5,
-        count: 56
-    },
-    price: 799
-}
-];
-
-
 document.querySelector('.products-grid').innerHTML = '';
 
 products.forEach((value)=>{
@@ -50,7 +20,7 @@ products.forEach((value)=>{
     </div>
 
     <div class="product-price">
-      $${(value.price/100).toFixed(2)}
+      $${(value.priceCents/100).toFixed(2)}
     </div>
 
     <div class="product-quantity-container">
