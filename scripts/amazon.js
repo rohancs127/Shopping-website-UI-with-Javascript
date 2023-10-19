fea379139e40045076dc09a1cdcbe46c9af3816f
Rohan <rohankumar5803@gmail.com>
2023-10-19 20:10:34 +0530

    ganerative html written in js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -20,9 +20,66 @@ const products =[{
     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
     rating:{
-        stars: 4.9,
+        stars: 4.5,
         count: 56
     },
     price: 799
 }
-]
+];
+
+
+document.querySelector('.products-grid').innerHTML = '';
+
+products.forEach((value)=>{
+    const productContainer = `<div class="product-container">
+    <div class="product-image-container">
+      <img class="product-image"
+        src="${value.image}">
+    </div>
+
+    <div class="product-name limit-text-to-2-lines">
+        ${value.name}
+    </div>
+
+    <div class="product-rating-container">
+      <img class="product-rating-stars"
+        src="images/ratings/rating-${value.rating.stars*10}.png">
+      <div class="product-rating-count link-primary">
+        ${value.rating.count}
+      </div>
+    </div>
+
+    <div class="product-price">
+      $${(value.price/100).toFixed(2)}
+    </div>
+
+    <div class="product-quantity-container">
+      <select>
+        <option selected value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+
+    <div class="product-spacer"></div>
+
+    <div class="added-to-cart">
+      <img src="images/icons/checkmark.png">
+      Added
+    </div>
+
+    <button class="add-to-cart-button button-primary">
+      Add to Cart
+    </button>
+  </div>
+`;
+
+document.querySelector('.products-grid').innerHTML += productContainer;
+})
