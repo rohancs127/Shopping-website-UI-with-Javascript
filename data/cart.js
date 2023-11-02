eabc1de0b9f0f7ec69d8e66e1a783d01765dc731
Rohan <rohankumar5803@gmail.com>
2023-11-02 21:41:40 +0530

    moved addtocart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,37 @@
 export let cart = [];
+
+export const addToCart =(productId) =>{
+
+    const quantitySelected = Number(document.querySelector(`.quantity-selector-${productId}`).value);
+  
+    let matchingItem;
+  
+      cart.forEach((value)=>{
+        if(value.productId===productId){
+        matchingItem=value;
+        }
+      })
+      
+  
+      if(matchingItem){
+        matchingItem.quantity +=quantitySelected;
+      }
+      else{
+      cart.push({
+        productId: productId,
+        quantity: quantitySelected
+      })
+    }
+  
+  
+    let cartQuantity = 0;
+  
+    cart.forEach((value)=>{
+      cartQuantity+= value.quantity;
+    })
+  
+    document.querySelector('.js-cart-quantity').innerHTML= cartQuantity;
+    
+  }
+  
+ 
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 document.querySelector('.products-grid').innerHTML = '';
@@ -57,40 +57,6 @@ products.forEach((value)=>{
 document.querySelector('.products-grid').innerHTML += productContainer;
 });
 
-
-const addToCart =(productId) =>{
-
-  const quantitySelected = Number(document.querySelector(`.quantity-selector-${productId}`).value);
-
-  let matchingItem;
-
-    cart.forEach((value)=>{
-      if(value.productId===productId){
-      matchingItem=value;
-      }
-    })
-    
-
-    if(matchingItem){
-      matchingItem.quantity +=quantitySelected;
-    }
-    else{
-    cart.push({
-      productId: productId,
-      quantity: quantitySelected
-    })
-  }
-
-
-  let cartQuantity = 0;
-
-  cart.forEach((value)=>{
-    cartQuantity+= value.quantity;
-  })
-
-  document.querySelector('.js-cart-quantity').innerHTML= cartQuantity;
-  
-}
 
 document.querySelectorAll('.add-to-cart-button').forEach((button)=>{
   button.addEventListener('click', ()=>{
